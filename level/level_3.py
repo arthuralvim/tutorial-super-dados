1a39a82d5aebf5fc68436aab2f124f48eb06b88e
Arthur Alvim <afmalvim@gmail.com>
2019-10-24 07:22:12 -0300

    Subindo respostas level-3.

--- level/level_3.py
+++ level/level_3.py
@@ -35,7 +35,18 @@ class SomaKoopaTroopaSemAsa(Level3Mixin, KoopaTroopaEnemies, luigi.Task):
                            'soma-koopatroopa-sem-asas.txt')
 
     def run(self):
-        NotImplemented
+        koopas = [enemy.to_dict() for enemy in self.enemies]
+        v = []
+        for koopa in koopas:
+            with ZipFile(koopa.get('enemy_file'), 'r') as zipf:
+                for name in zipf.namelist():
+                    with tempfile.TemporaryDirectory() as tempd:
+                        zipf.extract(name, tempd)
+                        with open(os.path.join(tempd, name), 'r') as f:
+                            v.append(int(f.read()))
+
+        with self.output().open('w') as o:
+            o.write(str(sum(v)))
 
 
 class SomaKoopaTroopaComAsa(Level3Mixin, KoopaTroopaWithWingsEnemies, luigi.Task):
@@ -45,7 +56,20 @@ class SomaKoopaTroopaComAsa(Level3Mixin, KoopaTroopaWithWingsEnemies, luigi.Task
                            'soma-koopatroopa-com-asas.txt')
 
     def run(self):
-        NotImplemented
+        koopas = [enemy.to_dict() for enemy in self.enemies]
+        v = []
+        for koopa in koopas:
+            with tempfile.NamedTemporaryFile(mode='wb') as temp:
+                self.s3_client.get(koopa.get('enemy_file'), temp.name)
+                with ZipFile(temp.name, 'r') as zipf:
+                    for name in zipf.namelist():
+                        with tempfile.TemporaryDirectory() as tempd:
+                            zipf.extract(name, tempd)
+                            with open(os.path.join(tempd, name), 'r') as f:
+                                v.append(int(f.read()))
+
+        with self.output().open('w') as o:
+            o.write(str(sum(v)))
 
 
 class SomarKoopaTroopa(Level3Mixin, LevelEnemies, luigi.Task):
@@ -61,7 +85,13 @@ class SomarKoopaTroopa(Level3Mixin, LevelEnemies, luigi.Task):
                            f'soma-total-koopatroppa.txt')
 
     def run(self):
-        NotImplemented
+        v = []
+        for i in self.input():
+            with open(i.path, 'r') as f:
+                v.append(int(f.read()))
+
+        with self.output().open('w') as o:
+            o.write(str(sum(v)))
 
 
 class Level3(Level3Mixin, LevelEnemies, luigi.WrapperTask):
